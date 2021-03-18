2736d2c93559ddbdbf801080945ea4a7fb377ed9
Lucas-Souza-Santos <lucas.ssantos2@ufpe.br>
2021-03-18 13:28:29 -0300

    Create Aula03.sql

---
+++ Aula03.sql
@@ -0,0 +1,8 @@
+-- CREATE DATABASE db_Biblioteca -- cria o banco de dados
+
+-- USE db_Biblioteca -- usa para entrar dentro do banco de dados criado
+
+sp_helpdb db_Biblioteca  -- mostra as informações do banco de dados
+
+
+
