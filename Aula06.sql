595380bc19c73aabc3de3fd7f4d99c6014891104
Lucas-Souza-Santos <lucas.ssantos2@ufpe.br>
2021-03-18 13:28:56 -0300

    Create Aula06.sql

---
+++ Aula06.sql
@@ -0,0 +1,7 @@
+/*			Auto Incremento
+	- O auto incremento permite que um número único seja gerado quando um novo registro é inserido em uma tabela.
+	- Em sql server trata-se da palavra chave identity(identidade), cujo valor inicial padrão é 1, e se incrementa em 1.
+	- Para que o valor de IDENTITY inicie em 100 e se incremente de 2 em 2, use IDENTITY(100, 2).
+*/	
+
+
